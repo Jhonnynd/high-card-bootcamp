9927f0ccbdbe13f61de9f39e0be2488d4a442a73
Jhonnynd <jhonnynd@gmail.com>
2023-05-31 15:52:57 +0800

    base game finished

--- src/App.js
+++ src/App.js
@@ -11,16 +11,43 @@ class App extends React.Component {
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
       currCards: [],
+      scoreboard: [0, 0],
+      startedGame: false,
+      roundWinner: 0,
     };
   }
 
   dealCards = () => {
+    let updatedScoreboard = [...this.state.scoreboard];
+    let updatedCurrCards = this.state.cardDeck.slice(-2);
+    let newRoundWinner = 0;
+
+    if (updatedCurrCards[0].rank > updatedCurrCards[1].rank) {
+      newRoundWinner = 1;
+      updatedScoreboard[0] += 1;
+    } else {
+      newRoundWinner = 2;
+      updatedScoreboard[1] += 1;
+    }
+
     this.setState((state) => ({
-      // Remove last 2 cards from cardDeck
       cardDeck: state.cardDeck.slice(0, -2),
-      // Deal last 2 cards to currCards
-      currCards: state.cardDeck.slice(-2),
+      currCards: updatedCurrCards,
+      scoreboard: updatedScoreboard,
+      roundWinner: newRoundWinner,
+      startedGame: true,
     }));
+  };
+
+  resetGame = () => {
+    this.setState({
+      cardDeck: makeShuffledDeck(),
+      currCards: [],
+      gameStarted: false,
+      startedGame: 0,
+      roundWinner: 0,
+      scoreboard: [0, 0],
+    });
   };
 
   render() {
@@ -31,17 +58,56 @@ class App extends React.Component {
       </div>
     ));
 
+    const numRoundsLeft = this.state.cardDeck.length / 2;
+
     return (
       <div className="App">
         <header className="App-header">
           <h3>High Card 🚀</h3>
           {currCardElems}
+
+          {/* {console.log(this.state.currCards)}
+          {console.log(this.state.currCards[0]?.rank)} */}
           <br />
           <button onClick={this.dealCards}>Deal</button>
+          <p>
+            {this.state.startedGame && this.state.roundWinner > 0
+              ? `Player  ${this.state.roundWinner} has won!`
+              : ""}
+          </p>
+          <p>
+            {this.state.startedGame && this.state.scoreboard[0] > 0
+              ? `Player  1 has won a total of ${this.state.scoreboard[0]} games!`
+              : ""}
+          </p>
+          <p>
+            {this.state.startedGame && this.state.scoreboard[1] > 0
+              ? `Player  2 has won a total of ${this.state.scoreboard[1]} games!`
+              : ""}
+          </p>
+          <p>
+            {numRoundsLeft > 0 && numRoundsLeft < 26
+              ? `${numRoundsLeft} rounds left!`
+              : numRoundsLeft === 0 &&
+                this.state.scoreboard[0] > this.state.scoreboard[1]
+              ? `Player 1 has won the entire game!`
+              : numRoundsLeft === 0 &&
+                this.state.scoreboard[1] > this.state.scoreboard[0]
+              ? `Player 2 has won entire game!`
+              : this.state.startedGame &&
+                this.state.scoreboard[0] === this.state.scoreboard[0]
+              ? `IT WAS A TIE!`
+              : ""}
+          </p>
+          {numRoundsLeft === 0 ? (
+            <button onClick={this.resetGame}>Reset game!</button>
+          ) : (
+            ""
+          )}
+          <br />
         </header>
       </div>
     );
   }
 }
-
 export default App;
